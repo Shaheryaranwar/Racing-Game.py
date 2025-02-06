ccee79e5b726caa6809f8dfdf6573312c9c5c2dc
M.Shaheryar <150699914+Shaheryaranwar@users.noreply.github.com>
2025-02-06 19:13:50 +0500

    Update main.py
    Final change show message in top center

--- main.py
+++ main.py
@@ -1,16 +1,14 @@
 from turtle import Turtle, Screen
 import random
-
 is_race_on = False
 screen = Screen()
 screen.setup(width=600, height=500)
 user_bet = screen.textinput(title="Make your own Bet red,orange,blue,green.purple,pink.yellow", prompt="Which turtle will win the race? Enter your color")
-colors = ["red", "orange", "blue", 'green', 'yellow', 'purple', 'brown']
+colors = ["red", "blue", "orange", 'green', 'yellow', 'purple', 'pink']
 y_positions = [-70, -40, -10, 20, 50, 80, 110]
 all_turtles = []
 turtle_shapes = ["square", "turtle", "circle", "triangle"]# random shapes
 #id u dont want random shapes just remove the list and in for loop shape = random.choice(turtle_shapes) change it 
-
 for turtle_index in range(7):
     shape = random.choice(turtle_shapes) 
     new_turtle = Turtle(shape=shape)
@@ -18,10 +16,8 @@ for turtle_index in range(7):
     new_turtle.penup()
     new_turtle.goto(x=-290, y=y_positions[turtle_index])
     all_turtles.append(new_turtle)
-
 if user_bet:
     is_race_on = True
-
 while is_race_on:
     for turtle in all_turtles:
         if turtle.xcor() > 260:
@@ -30,13 +26,11 @@ while is_race_on:
             message = Turtle()
             message.hideturtle()
             message.penup()
-            message.goto(0, 0)
+            message.goto(0, 200)  # Position the message in the upper center of the screen
             if winning_color == user_bet:
-                message.write(f"You win! The {winning_color} turtle is the winner!", align="center", font=("Arial", 16, "normal"))
+                message.write(f"You win! The {winning_color} turtle is the winner 🏆🎉😊!", align="center", font=("Arial", 16, "normal"))
             else:
-                message.write(f"You lose! The {winning_color} turtle is the winner!", align="center", font=("Arial", 16, "normal"))
-
-        distance = random.randint(0, 10)
+                message.write(f"You lose! The {winning_color} turtle is the winner 😞💔👎!", align="center", font=("Arial", 16, "normal"))
+        distance = random.randint(0, 30)
         turtle.forward(distance)
-
 screen.exitonclick()
